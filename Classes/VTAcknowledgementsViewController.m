218d3700cc300633b5c31105a537be05871e44cb
IgnacioCh <ign_ch@hotmail.com>
2015-01-18 16:26:46 +0000

    Adding transition from the tableview

--- Classes/VTAcknowledgementsViewController.m
+++ Classes/VTAcknowledgementsViewController.m
@@ -303,6 +303,8 @@ static const CGFloat VTLabelMargin = 20;
     VTAcknowledgement *acknowledgement = self.acknowledgements[indexPath.row];
     VTAcknowledgementViewController *viewController = [[VTAcknowledgementViewController alloc] initWithTitle:acknowledgement.title text:acknowledgement.text];
     viewController.textViewFont = self.licenseTextViewFont;
+    [self presentViewController:acknowledgement animated:YES completion:NULL];
+
     //[self.navigationController pushViewController:viewController animated:YES];
 }
 
